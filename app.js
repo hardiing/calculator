67b668b706db887090119ba6242328b8718839d2
Hardiing <nathanharding02@gmail.com>
2024-09-15 21:13:45 -0600

    Some styling, fully functional with clicks

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ let num1
 let num2
 let operator
 let displayNumber
+let previousNumber
+let firstOp = true
+let editable = true
 
 let numberButtons = document.querySelectorAll(".number")
-let operatorButtons = document.getElementsByClassName("operator")
+let operatorButtons = document.querySelectorAll(".operator")
 let equalsBtn = document.getElementById("equal")
 let clearBtn = document.getElementById("clear")
 
@@ -52,22 +55,55 @@ function toInt(str) {
 
 numberButtons.forEach(btn => {
     btn.addEventListener("click", function() {
-        if (display.value === "0") {
-           display.value = toInt(btn.innerHTML) 
+        if (editable === true) {
+           display.value = toInt(btn.innerHTML)
+           editable = false
         } else {
             display.value += toInt(btn.innerHTML)
         }
+        // if (firstOp === false) {
+        //     num2 = toInt(display.value)
+        //     operator = btn.innerHTML // trying to call innerhtml of operator button from number button event handler
+        //     displayNumber = evaluate(num1, num2, operator)
+        //     display.value = displayNumber
+        //     previousNumber = toInt(display.value)
+        //     num1 = previousNumber
+        //     editable = true
+        // }
     })
 })
 
 operatorButtons.forEach(btn => {
     btn.addEventListener("click", function() {
-        num1 = display.value
-        operator = btn.innerHTML
-    }
+        if (firstOp === true) {
+            num1 = toInt(display.value)
+            operator = btn.innerHTML
+            firstOp = false
+            editable = true
+        } else {
+            num2 = toInt(display.value)
+            operator = btn.innerHTML
+            displayNumber = evaluate(num1, num2, operator)
+            display.value = displayNumber
+            previousNumber = toInt(display.value)
+            num1 = previousNumber
+            editable = true
+        }   
+    })
 })
 
+equalsBtn.addEventListener("click", function() {
+    num2 = toInt(display.value)
+    displayNumber = evaluate(num1, num2, operator)
+    display.value = displayNumber
+    editable = true
+    firstOp = true
+})
 
-// numberButtons.forEach((button) =>
-//     button.addEventListener('click', () => appendNumber(button.textContent))
-//   )
+clearBtn.addEventListener("click", function() {
+    num1 = 0
+    num2 = 0
+    operator = ""
+    display.value = "0"
+    firstOp = true
+})
